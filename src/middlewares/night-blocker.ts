14c7d320e512dc76bd4afc9f296400c99640985b
codewithelie <me@elyh.fr>
2026-09-07 16:29:31 +0200

    Ajout du middleware visitCounter et mise à jour de nightBlocker pour bloquer l'accès entre 14h et 16h

--- src/middlewares/night-blocker.ts
+++ src/middlewares/night-blocker.ts
@@ -2,8 +2,9 @@ import type { RequestHandler } from "express";
 
 export const nightBlocker: RequestHandler = (req, res, next) => {
   const hour = new Date().getHours();
+  console.log(`Current hour: ${hour}`);
 
-  if (hour >= 0 && hour < 6) {
+  if (hour >= 14 && hour < 16) {
     res.status(503).json({
       message: "Le serveur est en cours de maintenance",
       data: null,
