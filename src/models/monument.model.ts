109d72135dc82ee9016cee447e4e099ae1a279e8
codewithelie <me@elyh.fr>
2026-09-08 11:20:37 +0200

    Ajout du modèle Monument avec définition des attributs et initialisation

--- src/models/monument.model.ts
+++ src/models/monument.model.ts
@@ -1,2 +1,55 @@
 import { DataTypes, Model, type InferAttributes, type InferCreationAttributes, type CreationOptional } from 'sequelize'
 import { sequelize } from '../db/sequelize.js';
+
+export class Monument extends Model<InferAttributes<Monument>, InferCreationAttributes<Monument>> {
+    declare id: CreationOptional<number>;
+    declare title: string;
+    declare country: string;
+    declare city: string;
+    declare buildYear: number | null;
+    declare picture : string | null;
+    declare description: string | null;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+}
+Monument.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    country: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    buildYear: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    picture: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+}, {
+    sequelize,
+});
