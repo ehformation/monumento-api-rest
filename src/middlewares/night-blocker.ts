6a5597511617be3ccefd7eabad9d94763e776e73
codewithelie <me@elyh.fr>
2026-09-08 10:06:30 +0200

    Mise à jour du middleware nightBlocker pour bloquer l'accès entre 0h et 6h

--- src/middlewares/night-blocker.ts
+++ src/middlewares/night-blocker.ts
@@ -4,7 +4,7 @@ export const nightBlocker: RequestHandler = (req, res, next) => {
   const hour = new Date().getHours();
   console.log(`Current hour: ${hour}`);
 
-  if (hour >= 14 && hour < 16) {
+  if (hour >= 0 && hour < 6) {
     res.status(503).json({
       message: "Le serveur est en cours de maintenance",
       data: null,
