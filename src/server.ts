79d0d32e7097e2f63e577fe3cce56f180b6ab8d8
codewithelie <me@elyh.fr>
2026-09-10 11:00:14 +0200

    Ajout de la gestion de Socket.IO : intégration de la dépendance socket.io dans package.json et mise à jour de la structure du serveur pour utiliser http.createServer.

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,19 @@
-import express from "express";
-import { nightBlocker } from "./middlewares/night-blocker.js";
-import { logger } from "./middlewares/logger.js";
-import { visitCounter } from "./middlewares/visit-counter.js";
+import http from "node:http";
 import { env } from "./config/env.js";
-import { monumentRouter } from "./routes/monument.routes.js";
-import { authRouter } from "./routes/auth.routes.js";
+import { createApp } from "./app.js";
 
 // Import models to ensure they are registered with Sequelize
 import "./models/monument.model.js";
 import "./models/user.model.js";
 
 import { initDatabase } from "./db/sequelize.js";
-import { errorHandler } from "./middlewares/error-handler.js";
-import { requireAuth } from "./middlewares/require-auth.js";
-
+ 
 // Initialize the database
 await initDatabase();
 
-//Express app setup
-const app = express();
-app.use(express.json());
+const app = createApp();
+const server = http.createServer(app);
 
-// Middlewares
-app.use(nightBlocker);
-app.use(logger);
-app.use(visitCounter);
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Bienvenue sur l'API Monumento !");
-});
-// Routes for monuments
-app.use("/auth", authRouter);
-app.use("/monuments", requireAuth, monumentRouter);
-
-app.get("/visit", (req, res) => {
-  res.send(`Vous êtes le visiteur n° ${req.visit}`);
-});
-
-// Error Middlewares 
-app.use(errorHandler);
-
-app.listen(env.PORT, () => {
+server.listen(env.PORT, () => {
   console.log(`API démarrée sur http://localhost:${env.PORT}`);
 });
