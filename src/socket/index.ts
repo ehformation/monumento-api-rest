82f20e9dabbfc47a98cd6a81517351e8279bba0f
codewithelie <me@elyh.fr>
2026-09-10 11:14:39 +0200

    Ajout de la configuration CORS pour le serveur Socket.IO et création d'un fichier HTML de test pour la connexion Socket.IO

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,7 +2,7 @@ import type http from "node:http";
 import { Server } from "socket.io";
 
 export function setupSocketServer(server: http.Server) : Server {
-    const io = new Server(server);
+    const io = new Server(server, { cors: { origin: "*" } });
 
     io.on("connection", (socket) => {
         console.log(`Client connecté : ${socket.id}`);
